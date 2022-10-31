fe3ce6f93d64c08cec349fc4e851e3a4538925bc
Akruthi-1999 <112990978+Akruthi-1999@users.noreply.github.com>
2022-10-30 21:25:03 -0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,16 @@ more_movies = [
     {:title => 'Nomadland', :rating => 'R',
       :release_date => '19-Feb-2021'},
     {:title => 'CODA', :rating => 'PG-13',
-      :release_date => '13-Aug-2021'}
+     :release_date => '13-Aug-2021'},
+    {:title => 'Black Panther', :rating => 'PG-13',
+     :release_date => '02-May-2018'},
+    {:title => 'Turning Red', :rating => 'PG',
+     :release_date => '11-Mar-2022'},
+    {:title => 'Thor Ragnarok', :rating => 'PG-13',
+     :release_date => '17-Feb-2018'},
+    {:title => 'Toy Story - 4', :rating => 'G',
+     :release_date => '01-Oct-2019'}
+
   ]
   
   more_movies.each do |movie|
